b8200b33512dc451dce1ad73f24a3c82cb077349
Rafael Anguiano <rj.anguiano.23@gmail.com>
2021-10-29 09:31:59 -0500

    redoing build

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 const GifGridItem = ({id, title, url}) => {
 
     return (
-        <div className='card animate_animated animate__fadeInDown'>
+        <div className='card animate_animated animate__fadeIn'>
             <img src={url} alt={title}/>
             <p>{title}</p>
         </div>
